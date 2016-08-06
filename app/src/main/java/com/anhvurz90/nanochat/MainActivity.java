838c0ea085575e4912a2f3d69ddde1813ddb6a47
anhvurz90 <vu@einsights.com>
2016-08-06 16:11:32 +0700

    Allow to send message from app to firebase db

--- app/src/main/java/com/anhvurz90/nanochat/MainActivity.java
+++ app/src/main/java/com/anhvurz90/nanochat/MainActivity.java
@@ -2,6 +2,7 @@ package com.anhvurz90.nanochat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -16,9 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button sendBtn = (Button) findViewById(R.id.sendBtn);
-        EditText messageTxt = (EditText) findViewById((R.id.messageTxt));
+        final EditText messageTxt = (EditText) findViewById((R.id.messageTxt));
         ListView messageLst = (ListView) findViewById(R.id.messageLst);
 
         Firebase.setAndroidContext(this);
+        final Firebase ref = new Firebase("https://nanochat-9e1d4.firebaseio.com/Chat");
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              ChatMessage chat = new ChatMessage("puf", messageTxt.getText().toString());
+              ref.push().setValue(chat);
+              messageTxt.setText("");
+          }
+        });
     }
 }
