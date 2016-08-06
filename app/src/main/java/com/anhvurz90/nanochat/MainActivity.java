77d5a61fb62baacfb2733c665d2e9e56df4c6723
anhvurz90 <vu@einsights.com>
2016-08-06 15:35:01 +0700

    Add elements to layout

--- app/src/main/java/com/anhvurz90/nanochat/MainActivity.java
+++ app/src/main/java/com/anhvurz90/nanochat/MainActivity.java
@@ -2,6 +2,9 @@ package com.anhvurz90.nanochat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendBtn = (Button) findViewById(R.id.sendBtn);
+        EditText messageTxt = (EditText) findViewById((R.id.messageTxt));
+        ListView messageLst = (ListView) findViewById(R.id.messageLst);
     }
 }
